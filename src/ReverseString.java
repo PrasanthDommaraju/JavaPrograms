9b5b5c0822e009b2f159213f24af02b48a24ba0b
PrasanthDommaraju <prasanth.dommaraju@gmail.com>
2026-09-07 15:06:35 +0530

    Added comments

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -3,10 +3,12 @@ public class ReverseString {
     public static void main (String[] args) {
 
         String s = "Java";
+        //Converting string to array
         char[] arr = s.toCharArray();
         String rev = "";
         int len = s.length();
 
+        //for loop to read the string in reverse
         for(int i=(len-1);i>=0;i--) {
 //            rev=rev+s.charAt(i);
               rev=rev+arr[i];
